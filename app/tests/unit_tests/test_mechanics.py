878acda799c84b08a0ec2181884752176ea46efe
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-10-29 09:23:24 -0400

    more tests

--- app/tests/unit_tests/test_mechanics.py
+++ app/tests/unit_tests/test_mechanics.py
@@ -4,6 +4,16 @@ from app.mechanics import create_questions_list
 from app.data import data
 
 
+fake_question = {
+    "In Greek Mythology, who was the daughter of King Minos?": [
+        "Ariadne",
+        "Athena",
+        "Ariel",
+        "Alana",
+    ]
+}
+
+
 def test_create_questions_list():
     # GIVEN questions dict : data
     # WHEN a list [dict  of random questions] is created
@@ -13,5 +23,8 @@ def test_create_questions_list():
     assert isinstance(q_list, (list, dict)) is True
 
 
-def test_populate_round_answers():
+@pytest.mark.parametrize(
+    question, correct_question_index, correct_answer_index, [fake_question]
+)
+def test_populate_round_answers(question, correct_question_index, correct_answer_index):
     pass
